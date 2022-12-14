2b13de28e7be511409521c69cafd95c52859e6cc
William Sampaio <1997wbms@gmail.com>
2022-12-13 23:07:13 -0400

    update

--- scripts/script.sh
+++ scripts/script.sh
@@ -3,3 +3,6 @@
 echo "Content-Type: text/html"
 echo ""
 echo "<html><body><h1>Hello world!</h1><p>I am a SHELL Script :)</p></body></html>"
+echo ""
+echo "<h2>Ls in root dir:</h2>"
+echo "<pre>$(ls -lah /)</pre>"
